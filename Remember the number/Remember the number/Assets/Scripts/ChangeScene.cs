2a3ba88d9c66afb5e30be32cfc20b7e79080e0c7
Tomas Kubin <Kubin.Tom@email.cz>
2019-02-18 19:57:13 +0100

    init commit

---
+++ Remember the number/Remember the number/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeToPlayGame(int sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+
+    }
+
+}
+
